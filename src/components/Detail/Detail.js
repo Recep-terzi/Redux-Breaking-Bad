5f0e5be538dd8ab2deb48d621182aeda6f90fc09
Recep Terzi <recepterzi-67@hotmail.com>
2022-08-21 22:11:57 +0300

    update

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,7 +1,29 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "../Loading/Loading";
 const Detail = () => {
-  return <div>Detail</div>;
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { char_id } = useParams();
+  useEffect(() => {
+    axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters/${char_id}`)
+      .then((res) => res.data)
+      .then((data) => setChar(data[0]))
+      .finally(setLoading(false));
+  }, [char_id]);
+  return (
+    <div>
+      {loading && <Loading />}
+      {char && (
+        <div>
+          <h1>{char.name}</h1>
+          <img src={char.img} alt={char.name} style={{ width: "200px" }} />
+        </div>
+      )}
+      {char && <pre>{JSON.stringify(char, null, 2)}</pre>}
+    </div>
+  );
 };
 
 export default Detail;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,20 +4,23 @@ import Masonry from "react-masonry-css";
 import { fetchCharacters } from "../../redux/charactersSlice";
 import "./Home.Module.css";
 import Loading from "../Loading/Loading";
+
 import Error from "../Error/Error";
 import { Link } from "react-router-dom";
 const Home = () => {
   const characters = useSelector((state) => state.characters.items);
   const error = useSelector((state) => state.characters.error);
-  const isLoading = useSelector((state) => state.characters.isLoading);
+  const status = useSelector((state) => state.characters.status);
   const hasNextPage = useSelector((state) => state.characters.hasNextPage);
   const nextPage = useSelector((state) => state.characters.page);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchCharacters());
-  }, [dispatch]);
+    if (status === "idle") {
+      dispatch(fetchCharacters());
+    }
+  }, [dispatch, status]);
 
-  if (error) {
+  if (status === "failed") {
     return <Error message={error} />;
   }
   return (
@@ -30,7 +33,7 @@ const Home = () => {
       >
         {characters.map((character) => (
           <div key={character.char_id}>
-            <Link to="/character/2">
+            <Link to={`/character/${character.char_id}`}>
               <img
                 src={character.img}
                 alt={character.name}
@@ -42,8 +45,8 @@ const Home = () => {
         ))}
       </Masonry>
       <div style={{ padding: 30, textAlign: "center" }}>
-        {isLoading && <Loading />}
-        {hasNextPage && !isLoading && (
+        {status === "loading" && <Loading />}
+        {hasNextPage && status !== "loading" && (
           <button onClick={() => dispatch(fetchCharacters(nextPage))}>
             Load More : ({nextPage})
           </button>
